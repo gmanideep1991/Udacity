f5d0f42abad8bbd93353a65473bcccd041150fce
Manideep Gattamaneni <gmanideep1991@gmail.com>
2016-07-18 16:10:28 -0500

    updated the game and readme. Initial commit.

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+The goal of this game is to get the player to reach the water without hitting any of the bugs.
+* If the player reaches water , gets one point.
+* If the player is hit by bug , reduces health.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+Once the player loses all health then the game is over.
+
+Movement
+--------
+User can move the player using arrow keys.
+* UP : Moves player one step up.
+* DOWN : Moves Player one step down.
+* RIGHT : Moves Player one step right.
+* LEFT : Moves player one step left.
+
+References
+----------
+* [Math.random()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+* [document.createElement()](https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement)
+* [Node](https://developer.mozilla.org/en-US/docs/Web/API/Node)
--- js/app.js
+++ js/app.js
@@ -1,39 +1,130 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var NUMBER_OF_ENEMIES = 3,
+    ENEMY_STARTING_POSITION = 60,
+    POSITION_INCREAMENT = 80,
+    ROW_WIDTH = 83,
+    COL_WIDTH = 101,
+    SCORE = 0,
+    PLAYER_LIFE = 2,
+    MAX_SPEED=600,
+    MIN_SPEED=0,
+    PLAYER_STARTING_POSITION_X=200,
+    PLAYER_STARTING_POSITION_Y=380;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+
+
+var Enemy = function(x, y) {
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    if (this.x < 500) {
+        this.x += getSpeed() * dt;
+    } else {
+        this.x += -100;
+        this.x = getSpeed() * dt;
+    }
 };
 
-// Draw the enemy on the screen, required method for game
+
+/**
+ * [getSpeed calculates the speed of enemy]
+ * @return {[number]} [random number between 0 and 600]
+ */
+function getSpeed() {
+    return Math.floor(Math.random() * (MAX_SPEED-MIN_SPEED+1) + MIN_SPEED);
+
+}
+
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+var allEnemies = [];
+for (i = 0; i < NUMBER_OF_ENEMIES; i++) {
+  //I am currently considering creating only three enemies i might refactor in future to support increase in enemies.
+    var temp = new Enemy(0, getPositionY(i));
+    allEnemies.push(temp);
+}
+
+/**
+ * [getPositionY used in calculating 'Y' co-ordinate for enmey while creating the enemy]
+ * @param  {[number]} i [column of enemy.]
+ * @return {[number]}   [returns the 'Y' co-ordinate.]
+ */
+function getPositionY(i) {
+    return (ENEMY_STARTING_POSITION + POSITION_INCREAMENT * i);
+}
+
+var Player = function(x, y) {
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
+    updatePlayerLife();
+    updateScoreBoard();
+};
+Player.prototype.update = function() {
+    for (var i = 0; i < allEnemies.length; i++) {
+        if ((this.y == allEnemies[i].y) && (this.x < allEnemies[i].x + ROW_WIDTH) && (this.x + ROW_WIDTH > allEnemies[i].x)) {
+            if (PLAYER_LIFE === 0) {
+              alert("Game Over!!!. Your score is '"+SCORE+"'. Hit Enter to play again.");
+              location.reload();
+            } else {
+                this.reset();
+                PLAYER_LIFE -= 1;
+                updatePlayerLife();
+            }
+
+        }
+    }
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.reset = function() {
+    this.x = PLAYER_STARTING_POSITION_X;
+    this.y = PLAYER_STARTING_POSITION_Y;
+};
+
+Player.prototype.handleInput = function(key) {
+    // make sure player is within boundaries of canvas
+    // make player move with appropriate keyboard key presse
+    switch (key) {
+        case 'left':
+            if (this.x > 0) {
+                this.x -= COL_WIDTH;
+            }
+            break;
+        case 'right':
+            if (this.x < 400) {
+                this.x += COL_WIDTH;
+            }
+            break;
+        case 'up':
+            if (this.y > 0) {
+                this.y -= POSITION_INCREAMENT;
+                if (this.y < 60) {
+                    this.reset();
+                    SCORE += 1;
+                    updateScoreBoard();
+                }
+            }
+            break;
+        case 'down':
+            if (this.y<380) {
+                this.y += POSITION_INCREAMENT;
+            }
+            break;
+        default:
+
+    }
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+};
+var player = new Player(PLAYER_STARTING_POSITION_X, PLAYER_STARTING_POSITION_Y);
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -44,3 +135,17 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+function updateScoreBoard(){
+  var nodeScore = document.getElementsByClassName("score")[0];
+  if(nodeScore.hasChildNodes()){
+    nodeScore.removeChild(nodeScore.childNodes[0]);
+  }
+    nodeScore.appendChild(document.createTextNode(SCORE));
+}
+function updatePlayerLife(){
+  var nodeScore = document.getElementsByClassName("player-life")[0];
+  if(nodeScore.hasChildNodes()){
+    nodeScore.removeChild(nodeScore.childNodes[0]);
+  }
+    nodeScore.appendChild(document.createTextNode(PLAYER_LIFE));
+}

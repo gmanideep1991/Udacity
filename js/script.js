b868163f2ccf60b5544c03108566e939bc29ae49
Manideep Gattamaneni <gmanideep1991@gmail.com>
2016-09-20 23:22:51 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,125 @@
+var locations = [{
+    query: "Kansas city power and light co",
+    lat: 39.099956,
+    long: -94.583673
+}, {
+    query: "H&R Block",
+    lat: 39.098166,
+    long: -94.582303
+}, {
+    query: "The Nelson-Atkins Museum of art",
+    lat: 39.048540,
+    long: -94.580644
+}, {
+    query: "National World War I Museum",
+    lat: 39.084734,
+    long: -94.585906
+}, {
+    query: "Baseball Museum",
+    lat: 39.094993,
+    long: -94.562904
+}];
+var map;
+
+function AppViewModel() {
+    var self = this;
+    this.locationList = ko.observableArray([]);
+    locations.forEach(function(val) {
+        self.locationList.push(new buildLocation(val));
+    });
+    this.searchTerm = ko.observable("");
+    this.filteredList = ko.computed(function() {
+        var filter = self.searchTerm().toLowerCase();
+        if (!filter) {
+            self.locationList().forEach(function(location) {
+                location.visible(true);
+            });
+            return self.locationList();
+        } else {
+            return ko.utils.arrayFilter(self.locationList(), function(location) {
+                var string = location.query.toLowerCase();
+                var result = (string.search(filter) >= 0);
+                location.visible(result);
+                return result;
+            });
+        }
+    }, self);
+}
+
+function buildLocation(data) {
+    var self = this;
+    this.visible = ko.observable(true);
+    this.query = data.query;
+    this.marker = buildMarker(data);
+    this.infoWindow = new google.maps.InfoWindow({
+        content: self.contentString
+    });
+    this.marker.addListener('click', function() {
+        loadContent(data, self);
+        self.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            self.marker.setAnimation(null);
+        }, 2100);
+    });
+    this.showMarker = ko.computed(function() {
+      this.marker.setMap(this.visible()=== true ? map : null);
+    }, this);
+}
+
+function buildMarker(data) {
+    return new google.maps.Marker({
+        position: new google.maps.LatLng(data.lat, data.long),
+        map: map
+    });
+}
+
+function loadContent(data, self) {
+
+    var clientId = "UEUQTTY4QUOVGYGCCFZN5BSPK0RINHAJOXMKXKI3ULALTT1W";
+    var clientSecret = "R1HU5A2BBMEKALYEQHHIBEYXU1VRUJ0SWALXN1MUDVJZG2MD";
+    var url = "https://api.foursquare.com/v2/venues/search";
+    $.getJSON(url, {
+        client_id: clientId,
+        client_secret: clientSecret,
+        v: 20160108,
+        query: data.query,
+        ll: data.lat + "," +
+            data.long,
+        limit: 1
+    }).done(function(data) {
+      var venue = data.response.venues[0];
+       var contentString ="<div class='info-window'>"+ getTitle(venue.name) + getUrl(venue.url) + getFormattedAddress(venue.location.formattedAddress)+ getPhoneNumber(venue.contact.formattedPhone)+"</div>";
+        self.infoWindow.setContent(contentString);
+        self.infoWindow.open(map,self.marker);
+    }).fail(function(data){
+      alert("Error in loading details from foursquare.");
+    });
+}
+
+function getFormattedAddress(formattedAddress){
+  var address = "";
+  for(i=0;i<formattedAddress.length;i++){
+    address += formattedAddress[i] + (i === formattedAddress.length-1 ? "":", ");
+  }
+  return "<div class=address>"+address+"<//div>";
+}
+
+function getTitle(name){
+  return "<div class=title>"+ name + "</div>";
+}
+function getUrl(url){
+  return url !==undefined ? '<div class="content"><a href="' + url +'">' + url + "</a></div>":"";
+}
+function getPhoneNumber(phoneNumber){
+  return '<div class="phone-number">' + phoneNumber + "</div>";
+}
+function initialize() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: {
+            lat: 39.076344,
+            lng: -94.579997
+        }
+    });
+    ko.applyBindings(new AppViewModel());
+}
